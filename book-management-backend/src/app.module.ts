70691a8e1473bbbeb62994f9ccdbc85642ceee0b
Sahan_SJ <sahansandeepa0003@gmail.com>
2025-11-06 19:28:12 +0530

    feat: fixing the route issues and improving the logics in the backend

--- book-management-backend/src/app.module.ts
+++ book-management-backend/src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 import { BooksModule } from './books/books.module';
 import { CategoriesModule } from './categories/categories.module';
 import { BorrowModule } from './borrow/borrow.module';
 
 @Module({
-  imports: [PrismaModule, BooksModule, CategoriesModule, BorrowModule],
+  imports: [
+    ConfigModule.forRoot(),
+    PrismaModule,
+    BooksModule,
+    CategoriesModule,
+    BorrowModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
